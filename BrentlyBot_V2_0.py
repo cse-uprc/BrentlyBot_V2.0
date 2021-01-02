6b9c01022c94068ee7422b903599bef120ac413c
JosueVanDyke <Josue.VanDyke@rockets.utoledo.edu>
2021-01-01 20:43:41 -0500

    Got screenshot command working

--- BrentlyBot_V2_0.py
+++ BrentlyBot_V2_0.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+import random
 import json
+import sys
+import pyautogui
 
 client = commands.Bot(command_prefix = "!")
 
-with open("keys.json") as json_file:  
+with open("keys.json") as json_file:
     data = json.load(json_file)
     TOKEN = data["discordKey"]
 
@@ -15,5 +18,18 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------")
+
+@client.event
+async def on_message(message):
+
+# Take a screenshot
+    if message.content.upper().startswith('!SCREENSHOT'):
+        
+        await message.channel.send('Your face!')
+
+        myScreenshot = pyautogui.screenshot()
+        myScreenshot.save(r'screencapture.png')
+
+        await message.channel.send(file=discord.File('screencapture.png'))
 
 client.run(TOKEN)
